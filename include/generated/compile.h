b1e77a114cc217c8225e153f91b124e1030c3507
MakeMeCool <sabyasachi2017@yahoo.com>
2017-03-28 20:31:47 +0000

    some changes

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm64"
+#define UTS_VERSION "#1 SMP PREEMPT Tue Mar 28 20:28:34 UTC 2017"
+#define LINUX_COMPILE_BY "Sam"
+#define LINUX_COMPILE_HOST "ReaperRig"
+#define LINUX_COMPILER "gcc version 4.9 20150123 (prerelease) (GCC) "
